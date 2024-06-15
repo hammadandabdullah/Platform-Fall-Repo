428ebea19fd374b007d7f885feefd3a2d23d987e
hammadandabdullah <32474042+hammadandabdullah@users.noreply.github.com>
2024-06-15 23:44:10 +0500

    fixed 3 issues

--- Assets/Scripts/VibratingPlatform.cs
+++ Assets/Scripts/VibratingPlatform.cs
@@ -5,22 +5,27 @@ using Unity.Netcode;
 
 public class VibratingPlatform : NetworkBehaviour
 {
-    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private float downSpeed = 10f;
 
     private float vibrationTime = 5f;
+    private float downTime = 1f;
     private float respawnTime = 1f;
     private Animator anim;
     private bool collided = false;
     private bool isVibrating = false;
+    private bool canGoDown = false;
     private bool isActive = true;
     private MeshRenderer meshRenderer;
     private MeshCollider meshCollider;
+    private Vector3 initialParentPosition;
 
     void Start()
     {
         anim = GetComponent<Animator>();
         meshRenderer = GetComponent<MeshRenderer>();
         meshCollider = GetComponent<MeshCollider>();
+
+        initialParentPosition = transform.parent.position;
     }
 
     private void Update()
@@ -31,7 +36,10 @@ public class VibratingPlatform : NetworkBehaviour
             collided = false;
         }
 
-        audioSource.enabled = isVibrating;
+        if (canGoDown)
+        {
+            transform.parent.position -= new Vector3(0, downSpeed * Time.deltaTime, 0);
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -53,10 +61,17 @@ public class VibratingPlatform : NetworkBehaviour
 
         if (!isVibrating)
         {
-            Invoke(nameof(TurnOffPlatform), vibrationTime);
-            Invoke(nameof(TurnOnPlatform), vibrationTime + respawnTime);
+            Invoke(nameof(GoDown), vibrationTime);
+            Invoke(nameof(TurnOffPlatform), vibrationTime + downTime);
+            Invoke(nameof(TurnOnPlatform), vibrationTime + downTime + respawnTime);
             isVibrating = true;
         }
+    }
+
+
+    private void GoDown()
+    {
+        canGoDown = true;
     }
 
     private void TurnOffPlatform()
@@ -64,6 +79,7 @@ public class VibratingPlatform : NetworkBehaviour
         meshRenderer.enabled = false;
         meshCollider.enabled = false;
         isActive = false;
+        canGoDown = false;
     }
 
     private void TurnOnPlatform()
@@ -75,6 +91,8 @@ public class VibratingPlatform : NetworkBehaviour
         isActive = true;
         isVibrating = false;
         collided = false;
+
+        transform.parent.position = initialParentPosition;
     }
 
     public bool IsActive()
